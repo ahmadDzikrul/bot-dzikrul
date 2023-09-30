10e7af87c81de9ccf4eabb10309c4e306a3d3d02
Ahmad Dzikrul Fikri <78804683+ahmadDzikrul@users.noreply.github.com>
2023-09-30 16:32:25 +0700

    add file bot.py, bot2.py, and bot_logic.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from bot_logic import gen_pass
+
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah masuk sebagai {client.user}')
+    print(gen_pass(10))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTE1NTA2MzkxMjY3NzUxOTQ2MA.G_mBmy.82Zzx8TRcvXY8ZJF3mX1dHMMyava9Npvv3oFWk")
---
+++ bot2.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def weekend(ctx):
+    await ctx.send(f'Hi aku {bot.user}, kamu kok hari sabtu malah les koding, gak liburan aja?')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTE1NTA2MzkxMjY3NzUxOTQ2MA.G_mBmy.82Zzx8TRcvXY8ZJF3mX1dHMMyava9Npvv3oFWk")
